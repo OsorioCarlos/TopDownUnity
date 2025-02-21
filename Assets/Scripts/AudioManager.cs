c2c353fe36d63f5ffaafe42e66c036c383b7178a
Carlos Osorio <cah.osorio@yavirac.edu.ec>
2025-02-21 00:11:19 -0500

    escenarios

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource sfxSource;
+
+    [SerializeField] private AudioClip backgroundMusic;
+
+    private void Start()
+    {
+        musicSource.clip = backgroundMusic;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+}
